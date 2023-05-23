3cf3f069eea9586145fed2f060252f850994105b
Saident <averilprimayuda@gmail.com>
2023-05-23 22:07:26 +0700

    Project Init with 2 Entity (User and Admin)

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         \App\Models\User::factory(1)->create();
+         \App\Models\Admin::factory(1)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'user@email.com',
+        //     'password' => '12345678',
+        // ]);
+
+        // \App\Models\ADmin::factory()->create([
+        //     'name' => 'Test Admin',
+        //     'email' => 'admin@email.com',
+        //     'password' => '12345678,'
+        // ]);
+    }
+}

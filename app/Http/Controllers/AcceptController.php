729720f734b08d2aff4012d12a336962439d5ba2
Saident <averilprimayuda@gmail.com>
2023-05-24 13:36:05 +0700

    AcceptController

--- app/Http/Controllers/AcceptController.php
+++ app/Http/Controllers/AcceptController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\SiteController;
 
 use App\Models\Admin;
 use App\Models\Kendaraan;
@@ -11,5 +12,30 @@ use App\Models\PemesananKendaraan;
 
 class AcceptController extends Controller
 {
-    //
+    //TODO : buat function konfirmasi + menolak
+    public function konfirmasiPesanan(Request $request){
+        $id = $request->id;
+        $kendaraans = Kendaraan::where('id', $id)->update([
+            'status' => 'Diproses'
+        ]);
+
+        return redirect()->action([\App\Http\Controllers\SiteController::class, 'dashboard']);
+    }
+    public function setujuiPesanan(Request $request){
+        $id = $request->id;
+        $kendaraans = Kendaraan::where('id', $id)->update([
+            'status' => 'Sedang Digunakan'
+        ]);
+
+        return redirect()->action([\App\Http\Controllers\SiteController::class, 'dashboard']);
+    }
+    public function tolakPesanan(Request $request){
+        $id = $request->id;
+        $kendaraans = Kendaraan::where('id', $id)->update([
+            'status' => 'Tersedia'
+        ]);
+        $kendaraans = PemesananKendaraan::where('kendaraan_id', $id)->delete();
+
+        return redirect()->action([\App\Http\Controllers\SiteController::class, 'dashboard']);
+    }
 }

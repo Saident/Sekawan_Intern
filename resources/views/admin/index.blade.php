58aaa8f6fdd6c7b3aac18e3444f54a54cf649b00
Saident <averilprimayuda@gmail.com>
2023-05-24 09:58:45 +0700

    TODO : add form to order

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,6 +10,10 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     Welcome to Admin Panel!
+
+                    <form method="POST" action="{{ route('admin.pesan') }}">
+                        
+                    </form>
                 </div>
             </div>
         </div>

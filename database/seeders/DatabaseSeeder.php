ac519f415fe82b65e413bb05e275ad5bd9b99d4e
Saident <averilprimayuda@gmail.com>
2023-05-24 18:37:01 +0700

    Update Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,12 +15,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(1)->create();
-         \App\Models\Admin::factory(1)->create();
+        //  \App\Models\User::factory(1)->create();
+        //  \App\Models\Admin::factory(1)->create();
+
+         \App\Models\User::factory()->create([
+            'name' => 'User1',
+            'email' => 'user1@email.com',
+            'email_verified_at' => now(),
+            'password' => '12345678', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'User2',
+            'email' => 'user2@email.com',
+            'email_verified_at' => now(),
+            'password' => '12345678', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\Admin::factory()->create([
+            'name' => 'Admin1',
+            'username' => 'Admin1',
+            'email' => 'admin1@email.com',
+            'email_verified_at' => now(),
+            'password' => '12345678', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\Admin::factory()->create([
+            'name' => 'Admin2',
+            'username' => 'Admin2',
+            'email' => 'admin2@email.com',
+            'email_verified_at' => now(),
+            'password' => '12345678', // password
+            'remember_token' => Str::random(10),
+        ]);
 
          \App\Models\Tambang::factory()->create([
-            'lokasi' => 'Jakarta',
+            'lokasi' => 'Surabaya',
             'user_id' => '1',
+        ]);
+
+        \App\Models\Tambang::factory()->create([
+            'lokasi' => 'Malang',
+            'user_id' => '2',
         ]);
 
         \App\Models\Kendaraan::factory()->create([
@@ -42,8 +82,17 @@ class DatabaseSeeder extends Seeder
             'jenis' => 'Angkutan Orang',
         ]);
 
+        \App\Models\Kendaraan::factory()->create([
+            'nama' => 'Truck D678',
+            'jenis' => 'Angkutan Barang',
+        ]);
+
         \App\Models\Driver::factory()->create([
-            'nama' => 'Averil Primayuda',
+            'nama' => 'Averil',
+        ]);
+
+        \App\Models\Driver::factory()->create([
+            'nama' => 'Primayuda',
         ]);
     }
 }
